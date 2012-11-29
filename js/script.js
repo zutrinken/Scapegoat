f6c39f4724038436d80ffb8b97f396e6b769b570
Peter Amende <peter.amende@hoccer.com>
2012-11-29 01:16:40 +0100

    Retina icons, new unorderd list stlye, main menu update, mobile detection queries for slider and bredcrumb, optimizing widgets, dynamic navigation fontsize

--- js/script.js
+++ js/script.js
@@ -1,13 +1,33 @@
 $(document).ready(function(){
+	/* Toggle Menu */
 	$('.menu-toggle').smoothScroll().click(function () {
 		$('#main-nav div').slideToggle('200');
 	});
+	
+	$(window).resize(arrr);
+	arrr();
+	function arrr() {
+		/* Navigation Font Size */
+		var holo = 0.8 + ($('#main-nav').width() / 2000);
+		$('#main-nav').css({'font-size': holo + 'em'});
+
+		/* Description Height */
+		var foo = $('#logo').height();
+		$('#description').css({'height': foo + 'px'});
+	}
+
+	/* Extra Class for smarter comment structure */
 	$('li.depth-1').has('ul.children').addClass('comment-group');
+
 	/* Dynamic equal width in Footer-Menu */
 	var n = 100 / ($('#footer_navigation ul li').length - $('#footer_navigation ul ul li').length);
 	$('#footer_navigation ul li').css('width', n + '%');
+
+	/* Responsive Youtube/Vimeo Videos */
 	$('.article').fitVids();
-	$('#front-page-slider').before('<a id="prev" class="toggling">&laquo;</a><a id="next" class="toggling">&raquo;</a>').cycle({
+
+	/* Slider on Frontpage */
+	$('#front-page-slider').before('<a id="prev" class="toggling">&laquo;</a><a id="next" class="toggling">&raquo;</a>').after('<nav id="slide-pager"></nav>').cycle({
 		activePagerClass	: 'activeSlide',
 		speed				: 1000,
 		timeout				: 6000,
@@ -15,13 +35,18 @@ $(document).ready(function(){
 		easeIn				: 'easeInOutQuad',
 		easeOut				: 'easeInOutQuad',
 		prev				: '#prev',
-		next				: '#next'
+		next				: '#next',
+		pager				: '#slide-pager'
 	});
+
+	/* Cookie if Slider is hidden */
 	if($.cookie('keks') == 'true') {
 		$('.toggling').hide();
 	} else {
 		$('.toggling').show();
 	};
+
+	/* Toggle the Slider */
 	$('#front-page-slider-toggle').click(function() {
 		if($('.toggling').css('display') == 'block') {
 			$.cookie('keks','true');
