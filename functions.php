c93fcb8ef3c45c18baf5d744730f886493ca430c
zutrinken <xoxo@peteramende.de>
2013-06-16 23:10:55 +0200

    added accesskey for menus. dirty copypasta from @Knutsen

--- functions.php
+++ functions.php
@@ -111,6 +111,43 @@ class My_Walker_Nav_Menu extends Walker_Nav_Menu {
 		if(isset($children[$el->$id])) $el->classes[] = 'has-children';
 		parent::display_element($el, $children, $max_depth, $depth, $args, $output);
 	}
+	function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+		global $wp_query;
+		$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+	
+		$class_names = $value = '';
+	
+		$classes = empty( $item->classes ) ? array() : (array) $item->classes;
+		$classes[] = 'menu-item-' . $item->ID;
+	
+		$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+		$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+	
+		$id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
+		$id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+	
+		$output .= $indent . '<li' . $id . $value . $class_names .'>';
+	
+		$attributes  = ! empty( $item->attr_title ) ? ' title="'  		. esc_attr( $item->attr_title ) .'"' : '';
+		$attributes .= ! empty( $item->target )     ? ' target="' 		. esc_attr( $item->target     ) .'"' : '';
+		$attributes .= ! empty( $item->xfn )        ? ' accesskey="'    . esc_attr( $item->xfn        ) .'"' : '';
+		$attributes .= ! empty( $item->url )        ? ' href="'   		. esc_attr( $item->url        ) .'"' : '';
+		
+		// First applying the filters. After that, underline the accesskey in the title if present...
+		$item_title = apply_filters( 'the_title', $item->title, $item->ID );
+		if( ! empty( $item->xfn ) ) {
+			$letterpos = strpos($item_title, esc_attr( $item->xfn ) );
+			$item_title = substr($item_title, 0, $letterpos)."<u>".substr($item_title, $letterpos, 1)."</u>".substr($item_title, $letterpos+1);
+		}
+		
+		$item_output = $args->before;
+		$item_output .= '<a'. $attributes .'>';
+		$item_output .= $args->link_before . $item_title . $args->link_after;
+		$item_output .= '</a>';
+		$item_output .= $args->after;
+	
+		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+	}
 }
 
 /* register sidebars */
