6e1a2b4a5ed94cf702d97e7813e260dceda9538e
Wolfgang Wiese <xwolf@xwolf.de>
2015-01-31 23:33:37 +0100

    added function to make all links in content, menus and other regions relative. This will cure headache of admins,who wants to make the site avaible both for http and https ;)

--- functions.php
+++ functions.php
@@ -816,4 +816,77 @@ function custom_comment($comment, $args, $depth) {
 		</div><!--comment-body-->
 		<?php
 }
-?>
+
+
+
+ function scapegoat_relativeimgurl($content){
+        return preg_replace_callback('/<img[^>]+/', 'scapegoat_relativeimgurl_callback', $content);
+}
+function scapegoat_relativeimgurl_callback($matches) {
+        $link = $matches[0];
+        $site_link =  wp_make_link_relative(home_url());  
+        $link = preg_replace("%src=\"$site_link%i", 'src="', $link);                 
+        return $link;
+    }
+ add_filter('the_content', 'scapegoat_relativeimgurl');
+
+
+ function wpi_relativeurl($content){
+        return preg_replace_callback('/<a[^>]+/', 'wpi_relativeurl_callback', $content);
+    }
+ 
+function wpi_relativeurl_callback($matches) {
+        $link = $matches[0];
+        $site_link =  wp_make_link_relative(home_url());  
+        $link = preg_replace("%href=\"$site_link%i", 'href="', $link);                 
+        return $link;
+    }
+ add_filter('the_content', 'wpi_relativeurl');
+   
+
+add_action('template_redirect', 'rw_relative_urls');
+function rw_relative_urls() {
+    // Don't do anything if:
+    // - In feed
+    // - In sitemap by WordPress SEO plugin
+    if (is_admin() || is_feed() || get_query_var('sitemap')) {
+        return;
+    }
+    $filters = array(
+        'post_link',
+        'post_type_link',
+        'page_link',
+        'attachment_link',
+        'get_shortlink',
+        'post_type_archive_link',
+        'get_pagenum_link',
+        'get_comments_pagenum_link',
+        'term_link',
+        'search_link',
+        'day_link',
+        'month_link',
+        'year_link',
+        'script_loader_src',
+        'style_loader_src',
+    );
+    foreach ($filters as $filter) {
+        add_filter($filter, 'scapegoat_make_link_relative');
+    }
+}
+
+function scapegoat_make_link_relative($url) {
+    $current_site_url = get_site_url();   
+	if (!empty($GLOBALS['_wp_switched_stack'])) {
+        $switched_stack = $GLOBALS['_wp_switched_stack'];
+        $blog_id = end($switched_stack);
+        if ($GLOBALS['blog_id'] != $blog_id) {
+            $current_site_url = get_site_url($blog_id);
+        }
+    }
+    $current_host = parse_url($current_site_url, PHP_URL_HOST);
+    $host = parse_url($url, PHP_URL_HOST);
+    if($current_host == $host) {
+        $url = wp_make_link_relative($url);
+    }
+    return $url; 
+}
